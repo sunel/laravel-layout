cf44fe6b30c5062f43514c1479b31459198301a1
unknown <sunel.saminathan@Temp34.wearefmg.net>
2015-02-11 14:58:18 +0530

    added service provider

---
+++ src/Provider/ExtViewServiceProvider.php
@@ -0,0 +1,27 @@
+<?php namespace Ext\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ExtViewServiceProvider extends ServiceProvider {
+
+	/**
+	 * Bootstrap the application services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		//
+	}
+
+	/**
+	 * Register the application services.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+}
