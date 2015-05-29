f6fdaf77457eca11cdc42e9bc531b36ecd6ecf1c
Sunel <sunelbe@gmail.com>
2015-05-30 00:24:14 +0530

    removed useless code

--- src/Layout/Element.php
+++ src/Layout/Element.php
@@ -4,38 +4,6 @@ namespace Layout\Layout;
 
 class Element extends AbstractXml
 {
-    public function prepare($args)
-    {
-        switch ($this->getName()) {
-            case 'layoutUpdate':
-                break;
-            case 'layout':
-                break;
-            case 'update':
-                break;
-            case 'remove':
-                break;
-            case 'block':
-                $this->prepareBlock($args);
-                break;
-            case 'reference':
-                $this->prepareReference($args);
-                break;
-            case 'action':
-                $this->prepareAction($args);
-                break;
-            default:
-                $this->prepareActionArgument($args);
-                break;
-        }
-        $children = $this->children();
-        foreach ($this as $child) {
-            $child->prepare($args);
-        }
-
-        return $this;
-    }
-
     public function getBlockName()
     {
         $tagName = (string) $this->getName();
@@ -44,31 +12,5 @@ class Element extends AbstractXml
         }
 
         return (string) $this['name'];
-    }
-    public function prepareBlock($args)
-    {
-        $name = (string) $this['name'];
-        $className = (string) $this['class'];
-        $parent = $this->getParent();
-        if (isset($parent['name']) && !isset($this['parent'])) {
-            $this->addAttribute('parent', (string) $parent['name']);
-        }
-
-        return $this;
-    }
-    public function prepareReference($args)
-    {
-        return $this;
-    }
-    public function prepareAction($args)
-    {
-        $parent = $this->getParent();
-        $this->addAttribute('block', (string) $parent['name']);
-
-        return $this;
-    }
-    public function prepareActionArgument($args)
-    {
-        return $this;
     }
 }
