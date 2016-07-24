d18b935ad847ec385074bc58b840053af59d1c7a
Sunel Tr <sunelbe@gmail.com>
2016-07-24 18:46:34 +0530

    added grid add button

--- views/template/page/html/grid.blade.php
+++ views/template/page/html/grid.blade.php
@@ -2,6 +2,13 @@
   <div class="box">
       <div class="box-header">
         <h3 class="box-title">{{ $block->getGridTitle() }}</h3>
+        <div class="box-tools pull-right">
+	        @if($block->getAddUrl())
+          <a href="{{ $block->getAddUrl() }}" class="btn btn-block btn-info"><i class="fa fa-plus"></i>
+          Add
+          </a>
+          @endif
+	    </div>
       </div>
       <div class="box-body">
         {!! $block->getGridHtml() !!}
